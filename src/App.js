bd8c58e68e8b33deca8cfe91e43b449f7b973bca
pedrodias92 <pedro.dias92@hotmail.com>
2019-04-08 00:04:19 +0100

    add clarifai api, and signin e register forms

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import Navigation from "./components/navigation/Navigation";
 import Logo from "./components/logo/Logo";
 import ImageLinkForm from "./components/imageLinkForm/ImageLinkForm";
 import Rank from "./components/rank/Rank";
+import FaceRecognition from "./components/faceRecognition/FaceRecognition";
+import Sigin from "./components/signin/Sigin";
+import Register from "./components/register/Register";
 import Particles from "react-particles-js";
+import Clarifai from "clarifai";
+
+const app = new Clarifai.App({
+  apiKey: "557b30e7ba7c49e99b68e27c2306abf0"
+});
 
 const particlesOptions = {
   particles: {
@@ -34,15 +42,105 @@ const particlesOptions = {
 };
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageUrl: "",
+      box: {},
+      route: "signin",
+      isSignedIn: false
+    };
+  }
+
+  onInputChange = event => {
+    console.log(event.target.value);
+    this.setState({
+      input: event.target.value,
+      imageUrl: "",
+      box: {}
+    });
+  };
+
+  calculateFaceLocation = data => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
+    };
+  };
+
+  displayFaceBox = box => {
+    this.setState({ box: box });
+    console.log(box);
+  };
+
+  onButtonSubmit = () => {
+    console.log("click");
+
+    this.setState({
+      imageUrl: this.state.input
+    });
+
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(response => {
+        this.displayFaceBox(this.calculateFaceLocation(response));
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  onRouteChange = route => {
+    if (route === "signout") {
+      this.setState({
+        isSignedIn: false
+      });
+    } else if (route === "home") {
+      this.setState({
+        isSignedIn: true
+      });
+    }
+    this.setState({
+      route: route
+    });
+  };
+
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
         <Particles className="particles" params={particlesOptions} />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm />
-        {/* <FaceRecognition /> */}
+        <Navigation
+          isSignedIn={isSignedIn}
+          onRouteChange={this.onRouteChange}
+        />
+        {route === "home" ? (
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
+          </div>
+        ) : route === "signin" ? (
+          <div>
+            <Sigin onRouteChange={this.onRouteChange} />
+          </div>
+        ) : (
+          <div>
+            <Register onRouteChange={this.onRouteChange} />
+          </div>
+        )}
       </div>
     );
   }
--- src/components/imageLinkForm/ImageLinkForm.js
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -1,26 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 import "./ImageLinkForm.css";
 
-export default class ImageLinkForm extends Component {
-  render() {
-    return (
-      <div>
-        <p className="f3">
-          {"This Magic will detect faces in your pictures. Git ii a try"}
-        </p>
-        <div className="center">
-          <div className="form center pa4 br3 shadow-3 ">
-            <input
-              className="f4 pa2 w-70 center"
-              type="text"
-              placeholder="paste link"
-            />
-            <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
-              Detect
-            </button>
-          </div>
+export default function ImageLinkForm({ onInputChange, onButtonSubmit }) {
+  return (
+    <div>
+      <p className="f3">
+        {"This Magic will detect faces in your pictures. Git ii a try"}
+      </p>
+      <div className="center">
+        <div className="form center pa4 br3 shadow-3 ">
+          <input
+            className="f4 pa2 w-70 center"
+            type="text"
+            placeholder="paste link"
+            onChange={onInputChange}
+          />
+          <button
+            className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
+            onClick={onButtonSubmit}
+          >
+            Detect
+          </button>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
